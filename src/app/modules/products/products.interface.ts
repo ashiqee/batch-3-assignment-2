93b23bb2981395e030fbdf9ec9c8ee37986f294d
ashiq73 <ashiq.buet73@gmail.com>
2024-05-21 10:06:30 +0600

    products model and interface done

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,19 @@
+export type Variant = {
+  type: string;
+  value: string;
+};
+
+export type Inventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type Products = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variant: Variant[];
+  invetory: Inventory;
+};
---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,53 @@
+import { model, Schema } from "mongoose";
+import { Inventory, Products, Variant } from "./products.interface";
+
+const variantSchema = new Schema<Variant>({
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+
+const inventorySchema = new Schema<Inventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const productsSchema = new Schema<Products>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  variant: variantSchema,
+  invetory: inventorySchema,
+});
+
+
+
+export const ProductsModel = model<Products>("Products", productsSchema)
