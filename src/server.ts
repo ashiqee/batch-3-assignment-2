ef9bccd264006bb8d290029b9bb6bb4cfbc52ca3
ashiqee <ashiq.buet73@gmail.com>
2024-05-21 07:28:56 +0600

    intial server setup and mongoose connect

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+import config from "./app/config";
+
+async function main() {
+    await mongoose.connect(config.database_url as string);
+  
+    app.listen(config.port, () => {
+        console.log(`Ecommerce-DB Sever running on port ${config.port}`)
+      })
+
+  }
+
+
+  main()
